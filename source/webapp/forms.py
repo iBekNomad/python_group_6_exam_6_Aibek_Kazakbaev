e844ea6ca644e9d3e8889589948503f9f940f28e
Aibek <nomad.aibek@gmail.com>
2020-08-01 19:44:19 +0600

    Added guest form.

---
+++ source/webapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import STATUS_CHOICES
+
+
+default_status = STATUS_CHOICES[0][0]
+
+
+class GuestForm(forms.Form):
+    name = forms.CharField(max_length=40, required=True, label='Гость')
+    email = forms.EmailField(max_length=30, required=True, label='Электронная почта')
+    text = forms.CharField(max_length=2000, required=True, label='Текст')
